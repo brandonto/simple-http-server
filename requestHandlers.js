cc82db5c412e47cbe21797c81c9dbbf900c57960
Brandon To <brandon_to@hotmail.com>
2013-07-20 19:21:05 -0400

    Changed server to handle png uploads

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,9 @@
 var exec = require("child_process").exec;
 var fs = require("fs");
 var querystring = require("querystring");
+var formidable = require("formidable");
 
-function start(response, postData) {
+function start(response, request) {
 	console.log("Request handler 'start' was called.");
 	exec("ls -alh", function(error, stdout, stderr){
 		response.writeHead(200, {"Content-Type": "text/plain"});
@@ -11,21 +12,35 @@ function start(response, postData) {
 	});
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " + querystring.parse(postData).text);
-	response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+            if (error) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image: <br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function textpad(response, postData) {
+function textpad(response, request) {
 	console.log("Request handler 'textpad' was called.");
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write(fs.readFileSync("textpad.html").toString());
 	response.end();
 }
 
-function show(response, postData) {
+function show(response, request) {
     console.log("Request handler 'show' was called.");
     fs.readFile("/tmp/test.png", "binary", function(error, file) {
         if (error) {
